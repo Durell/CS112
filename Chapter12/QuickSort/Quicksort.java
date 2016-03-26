bd417a08a7a2b80f4234e54f2f9992dced1ddb16
durell <durell.s.smith@gmail.com>
2016-03-25 18:09:25 -0700

    update

--- Chapter12/QuickSort/Quicksort.java
+++ Chapter12/QuickSort/Quicksort.java
@@ -1,30 +1,76 @@
 /*
-Chapter No. 1x - Exercise No. ???
-File Name:          GoodTime.java
+Chapter No. 12 - Project 8
+File Name:          QuickSort.java
 Programmer:         Durell Smith
-Date Last Modified: March 2x, 2016
+Date Last Modified: March x, 2016
 
 Problem Statement:
+
+6. Recode the QuickSort class implementation by adding two efficiency improvements
+to the method sort : (1) Eliminate the calls to join , because it accomplishes
+nothing. (2) Add code for the special case of an array of exactly two elements and
+make the general case apply to arrays of three or more elements.
+
+7. Redo the QuickSort class so that it chooses the splitting point as follows: The
+splitting point is the middle (in size) of the first element, the last element, and an
+element at approximately the middle of the array. This will make a very uneven
+split less likely.
+
+8. Redo the QuickSort class to have the modifications given for Programming
+Projects 12.6 and 12.7.
+
 */
 
 // imports
 
-public class GoodTime
+public class QuickSort
 {
-	// variables
-
-	/**
-	* Default Constructor
+	/***
+	Precondition: Interval a[begin] through a[end] of a have elements.
+	Postcondition: The values in the interval have
+	 been rearranged so that a[begin] <= a[begin+1] <= . . . <=
+	 a[end].
 	*/
-	public GoodTime()
+	public static void sort( double[] a, int begin, int end)
 	{
+		if ((end - begin) >= 1)
+		{
+			int splitPoint = split(a, begin, end);
+			sort(a, begin, splitPoint);
+			sort(a, splitPoint + 1, end);
+		}//else sorting one (or fewer) elements so do nothing.
 	}
 
-	// accessor and mutator methods
-
-	// main method
-	public static void main(String[] args)
+	private static int split( double [] a, int begin, int end)
 	{
-	} // end main method
+		double[] temp;
+		int size = (end - begin + 1);
+		temp = new double [size];
+		double splitValue = a[begin];
+		int up = 0;
+		int down = size - 1;
 
-} // end GoodTime class
+		//Note that a[begin] = splitValue is skipped.
+		for ( int i = begin + 1; i <= end; i++)
+		{
+			if (a[i] <= splitValue)
+			{
+				temp[up] = a[i];
+				up++;
+			}
+			else
+			{
+				temp[down] = a[i];
+				down--;
+			}
+		}
+		//0 < = up = down < size
+		temp[up] = a[begin]; //Positions the split value
+		//temp[i] <= splitValue for i < up
+		// temp[up] = splitValue
+		// temp[i] > splitValue for i > up
+		for ( int i = 0; i < size; i++)
+			a[begin + i] = temp[i];
+		return (begin + up);
+	}
+}
--- StyleSheet.java
+++ StyleSheet.java
@@ -1,30 +1,19 @@
 /*
-Chapter No. 1x - Exercise No. ???
+Chapter No. 1x - Project x
 File Name:          GoodTime.java
 Programmer:         Durell Smith
-Date Last Modified: March 2x, 2016
+Date Last Modified: March x, 2016
 
 Problem Statement:
+
 */
 
 // imports
 
 public class GoodTime
 {
-	// variables
 
-	/**
-	* Default Constructor
-	*/
-	public GoodTime()
-	{
-	}
-
-	// accessor and mutator methods
-
-	// main method
-	public static void main(String[] args)
-	{
-	} // end main method
-
-} // end GoodTime class
+  public static void main(String[] args)
+  {
+  }
+}
