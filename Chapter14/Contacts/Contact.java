57410d5017dbbed9d58610d6744f6eb98158d130
durell <durell.s.smith@gmail.com>
2016-04-07 21:39:21 -0700

    semester framework

---
+++ Chapter14/Contacts/Contact.java
@@ -0,0 +1,31 @@
+/*
+Chapter No. 14 - Project 2 (Page 799)
+File Name:          Contact.java
+Programmer:         Durell Smith
+Date Last Modified: April x, 2016
+
+Problem Statement:
+
+FROM BOOK:
+Write a program that uses an ArrayList of parameter type Contact to store a
+database of contacts. The Contact class should store the contact’s first and last
+name, phone number, and email address. Add appropriate accessor and mutator
+methods. Your database program should present a menu that allows the user to
+add a contact, display all contacts, search for a specific contact and display it, or
+search for a specific contact and give the user the option to delete it. The searches
+should find any contact where any instance variable contains a target search string.
+For example, if “elmore” is the search target, then any contact where the first name,
+last name, phone number, or email address contains “elmore” should be returned
+for display or deletion. Use the “for-each” loop to iterate through the ArrayList .
+
+*/
+
+// imports
+
+public class Contact
+{
+
+  public static void main(String[] args)
+  {
+  }
+}
---
+++ Chapter15/LinkedLists/LinkedLists.java
@@ -0,0 +1,25 @@
+/*
+Chapter No. 15 - Project 6 (Page 895)
+File Name:          LinkedLists.java
+Programmer:         Durell Smith
+Date Last Modified: April x, 2016
+
+Problem Statement:
+
+FROM BOOK:
+Write an addSorted method for the generic linked list from Display 15.8(p.818)
+such that the method adds a new node in the correct location so that the list 
+remains in sorted order. Note that this will require that the type parameter T 
+extend the Comparable interface. Write a suitable test program.
+
+*/
+
+// imports
+
+public class LinkedLists
+{
+
+  public static void main(String[] args)
+  {
+  }
+}
